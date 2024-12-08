ea869d95681b7ab3bc30379c83af567e252b24b7
harinarayanan173 <harinarayanan173@gmail.com>
2024-12-08 21:41:57 +0530

    Add files via upload

---
+++ chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// datatypes variablename = Values;
+		float typeofversion = 91.0f;
+		String typeofdeveloper = "Google";
+		Boolean typeofbeta = false;
+		int typeofreleaseyear = 2008;
+		char typeofshortcutKey = 'C';
+		System.out.println(typeofversion);
+		System.out.println(typeofdeveloper);
+		System.out.println(typeofbeta);
+		System.out.println(typeofreleaseyear);
+		System.out.println(typeofshortcutKey);
+	
+	}
+
+}
